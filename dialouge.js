fca7a3dcfdbdbdb6e7c28fc8207039c4a8a1b824
TheCasualNerd <tyler.duck02@gmail.com>
2017-06-18 17:10:27 -0400

    Update dialouge.js

--- dialouge.js
+++ dialouge.js
@@ -1,9 +1,7 @@
-angular.module('dialouge', []).factory('tbateamsearch', function ($http) {
-  var baseURL = 'http://www.thebluealliance.com/api/v3';
-
-
-
-
-
-
-});
+const dataKey = 'u2kDTQPh5Kgn7cOTKTsIzmqoT0KsXBq3PO5U0BeSQgSb7k1kakV6ElfYC0pLCBEf'
+export default new Class info {
+    queryApi() {
+      return fetch().then(j => j.json());
+    }
+}
+https://github.com/calebeby/frc-match-prep/blob/9cb1150aead1f587c0d42c082127fd0f8c523e9b/assets/js/data.js
