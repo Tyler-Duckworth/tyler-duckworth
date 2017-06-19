52641800bbf84a708f34e76d956d772b49ad1aba
TheCasualNerd <tyler.duck02@gmail.com>
2017-06-19 15:47:57 -0400

    Update dialouge.js

--- dialouge.js
+++ dialouge.js
@@ -1,7 +1,1 @@
-const dataKey = 'u2kDTQPh5Kgn7cOTKTsIzmqoT0KsXBq3PO5U0BeSQgSb7k1kakV6ElfYC0pLCBEf'
-export default new Class info {
-    queryApi() {
-      return fetch().then(j => j.json());
-    }
-}
-https://github.com/calebeby/frc-match-prep/blob/9cb1150aead1f587c0d42c082127fd0f8c523e9b/assets/js/data.js
+
